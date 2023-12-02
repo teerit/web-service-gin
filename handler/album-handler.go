e8086b73496b00ec0ffc4f65b3fe5a00ff398c06
Teerit Ploensin <teeritploensin@gmail.com>
2023-12-02 16:20:12 +0700

    add post add album

--- handler/album-handler.go
+++ handler/album-handler.go
@@ -7,8 +7,23 @@ import (
 	"github.com/teerit/web-service-gin/model"
 )
 
+// get Albums responds with the list of all albums as JSON.
 func GetAlbums(c *gin.Context) {
 	// gin.Context carries request details, validates and serializes JSON, and more.
 	// Call Context.IndentedJSON to serialize the struct into JSON
 	c.IndentedJSON(http.StatusOK, model.Albums)
+}
+
+// postAlbums adds an album from JSON received in the request body.
+func PostAlbums(c *gin.Context) {
+	var newAlbum model.Album
+
+	// Call BindJSON to bind the received JSON to
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	// Add the new album to the slice
+	model.Albums = append(model.Albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
--- main.go
+++ main.go
@@ -10,6 +10,7 @@ func main() {
 	// GET function to associate the GET HTTP method and /albums path with
 	// a handler function
 	router.GET("/albums", handler.GetAlbums)
+	router.POST("/albums", handler.PostAlbums)
 
 	router.Run("localhost:8080")
 }
